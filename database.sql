c69bb27344ae9ec5e5d72e616df777f0cc15db71
Alunggu Simatupang <155988222+SmokeyBeef234@users.noreply.github.com>
2024-06-12 09:51:50 +0700

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,28 @@
+create TABLE barang(
+	barangID INT NOT NULL AUTO_INCREMENT,
+	barangName VARCHAR(100) NOT NULL,
+	barangHarga FLOAT(40) NOT NULL,
+	barangStock INT NOT NULL,
+	barangDesc VARCHAR(200),
+	PRIMARY KEY ( barangID )
+);
+
+insert into barang 
+(barangName, barangHarga, barangStock, barangDesc)
+values
+("Kaos", 300000, 2, "Kaos Lengan Panjang"),
+("Kemeja", 250000, 2, "Kemeja kain"),
+("Topi", 80000, 20, "Topi NB"),
+("Sepatu", 300000, 3, "Sepatu compass"),
+("Kaos Kaki", "20000", 50, "Kaos kaki pria");
+
+
+select * from barang
+
+select barangName , barangHarga , barangDesc from barang
+
+select b.barangName , b.barangHarga  from barang b
+where barangName = "Kaos"
+
+update barang set barangName = "Topi Rapper"
+where barangStock = 20
